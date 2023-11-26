251b71f36d60f8dab0302fea57ff2f6819035e58
nazifa <nazifazahan9@gmail.com>
2023-11-26 14:44:21 +0000

    starting coursework

---
+++ src/SocMember.java
@@ -0,0 +1,33 @@
+//Question 1
+public class SocMember {
+    private String name;
+    private int number;
+
+    public static int student_counter = 0; //this is a tracker for the amount of members in the society
+
+    // set and get for name
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    //set and get for member number
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    //constructor
+    public SocMember(String name, int number){
+        this.name = name;
+        this.number = number;
+        student_counter++; // this ensures that the student counter is incremented when a new member is added
+    }
+
+}
