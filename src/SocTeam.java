2da4f90b02eafd178ac57c990fade1f97e54ef71
nazifa <nazifazahan9@gmail.com>
2023-11-27 12:43:08 +0000

    trying questions 2 part a (its not working for some reason)

--- src/SocTeam.java
+++ src/SocTeam.java
@@ -2,4 +2,11 @@ public class SocTeam {
     private String president;
     private String secretary;
     private String treasurer;
+
+    public SocTeam(president, secretary, treasurer) {
+        this.president = president;
+        this.secretary = secretary;
+        this.treasurer = treasurer;
+
+    }
 }
