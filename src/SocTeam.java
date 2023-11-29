f7392761f4ab3e0ae26c05f9c7da5e3659b6a6df
nazifa <nazifazahan9@gmail.com>
2023-11-29 16:05:16 +0000

    trying questions 2 part a (its not working for some reason)

--- src/SocTeam.java
+++ src/SocTeam.java
@@ -1,12 +1,26 @@
 public class SocTeam {
-    private String president;
-    private String secretary;
-    private String treasurer;
+    private SocMember president;
+    private SocMember secretary;
+    private SocMember treasurer;
 
-    public SocTeam(president, secretary, treasurer) {
+    public SocTeam(SocMember president, SocMember secretary, SocMember treasurer) {
         this.president = president;
         this.secretary = secretary;
         this.treasurer = treasurer;
 
+    }
+
+    // getter methods to return the name of a member depending on their role
+    // this refers back to SocMember getters to get the names of the soc team
+    public String getPresidentName() {
+        return president.getName();
+    }
+
+    public String getSecretary() {
+        return secretary.getName();
+    }
+
+    public String getTreasurer() {
+        return treasurer.getName();
     }
 }
